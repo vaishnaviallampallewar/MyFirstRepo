a43542338fe58ac3c64f34db139f46eff1fafd7b
Vaishnavi Allampallewar <vaishnavi_a@persistent.co.in>
2017-05-30 13:40:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
   public static void task(){
-    System.out.println("inside task method")
-  
+    System.out.println("inside task method");
+  }
   public static void main(String[] args){
      System.out.println("Hello to all from Vaishnavi!");
   }
